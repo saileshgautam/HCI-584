018de89db36ff493e030e970002c119994258bee
saileshgautam <80487723+saileshgautam@users.noreply.github.com>
2023-11-07 00:23:04 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def search_paper_by_author(author_name):
     results = sch.search_author(author_name, limit=1000)
     print(f"{len(results.raw_data)} authors found for {author_name}")
 
-    data = results.raw_data  # raw_data is a bit of a hack, but it works. 0 for only the first author
+    data = results.raw_data
 
     affiliations = 'Iowa State University'
 
@@ -32,26 +32,30 @@ def search_paper_by_author(author_name):
     paper_without_openaccess = []
     
     for item in paper:
-        if item['isOpenAccess'] == True:
+        if item['isOpenAccess'] == True and item['openAccessPdf'] is not None:
             paper_with_openaccess.append(item)
         else:
             paper_without_openaccess.append(item)
-    
+    paper_info = []
     for paper in paper_with_openaccess:
-        paper_info = []
-        paper_info.append(paper['title'])
-        return paper_info
+            paper_info.append({
+        "title": paper["title"],
+        "abstract": paper["abstract"],
+        "Downloadlink": paper['openAccessPdf']['url'],
+        "Link": paper['url']
+        })
+    return paper_info
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    author_name = "Stephen Gilbert"  # Replace with the author's name
-    if request.method == 'POST':
-        author_name = request.form['author_name']
-    paper_info = search_paper_by_author(author_name)
-    return render_template('index.html', author_name=author_name, paper_info = paper_info)
-
+    if request.method == "POST":
+        author_name = request.form["author_name"]
+        papers = search_paper_by_author(author_name)
+        html = render_template("index.html", paper_info=papers)
+        return html
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
